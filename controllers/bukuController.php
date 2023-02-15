9fcba0dccf4ed8ae370b7a9d4172493e5cfb7496
amin subagiyo <aminsum9@gmail.com>
2023-02-15 20:52:13 +0700

    update api buku

--- controllers/bukuController.php
+++ controllers/bukuController.php
@@ -18,7 +18,6 @@ class bukuController
 
     function create($nomor_buku, $nama_buku, $penerbit, $penulis, $tanggal_masuk)
     {
-        $db_conn = new Database();
 
         $nomor_buku = mysqli_real_escape_string($this->db_conn, trim($nomor_buku));
         $nama_buku = mysqli_real_escape_string($this->db_conn, trim($nama_buku));
@@ -29,9 +28,9 @@ class bukuController
         $addUser = mysqli_query($this->db_conn, "INSERT INTO `perpustakaan_botika`.buku(id,nomor_buku,nama_buku,penerbit,penulis,tanggal_masuk) VALUES(0,'$nomor_buku','$nama_buku','$penerbit'.'$penulis','tanggal_masuk')");
 
         if ($addUser) {
-            echo json_encode(["success" => true, 'success register']);
+            echo json_encode(["success" => true, "message" => 'success register']);
         } else {
-            echo json_encode(["success" => false, 'failed register']);
+            echo json_encode(["success" => false, "message" => 'failed register']);
         }
 
     }
@@ -44,17 +43,18 @@ class bukuController
         if (mysqli_num_rows($all_user) > 0) {
             $allUsers = mysqli_fetch_all($all_user, MYSQLI_ASSOC);
 
-            echo json_encode(['success' => 1, 'users' => $allUsers]);
+            echo json_encode(['success' => false, 'users' => $allUsers]);
         } else {
-            echo json_encode(['success' => 0]);
+            echo json_encode(['success' => false, "message" => "failed get data buku"]);
         }
 
     }
 
-    function update_buku($id,$nomor_buku, $nama_buku, $penerbit, $penulis, $tanggal_masuk)
+    function update($id,$nomor_buku, $nama_buku, $penerbit, $penulis, $tanggal_masuk)
     {
 
         if (isset($id) && isset($nomor_buku) && isset($nama_buku) && isset($penerbit) && isset($penulis)  && isset($tanggal_masuk)) {
+
             $updateUser = mysqli_query($this->db_conn, "
             UPDATE user SET nomor_buku = '$nomor_buku', 
             nama_buku = '$nama_buku', 
@@ -64,12 +64,13 @@ class bukuController
             WHERE id = $id");
 
             if ($updateUser) {
-                echo json_encode(["success" => 1, "buku $nama_buku updated successfully"]);
+                echo json_encode(["success" => true, "buku $nama_buku updated successfully"]);
             } else {
-                echo json_encode(["success" => 0, "buku $nama_buku failed updated!"]);
+                echo json_encode(["success" => false, "buku $nama_buku failed updated!"]);
             }
+            
         } else {
-            echo  json_encode(["success" => 0, "please fill all required data!"]);
+            echo  json_encode(["success" => false, "please fill all required data!"]);
         }
 
     }
@@ -78,15 +79,15 @@ class bukuController
     {
         
         if(isset($id)){
-            $deleteUser = mysqli_query($this->db_conn, "DELETE FROM buku WHERE id = $id");
+            $deleteUser = mysqli_query($this->db_conn, "DELETE FROM buku WHERE id =".$id);
         
             if($deleteUser){
-                echo json_encode(["success" => 1, "User success deleted!"]);
+                echo json_encode(["success" => true, "User success deleted!"]);
             } else {
-                echo  json_encode(["success" => 0, "User failed deleted!"]);
+                echo  json_encode(["success" => false, "User failed deleted!"]);
             }
         } else {
-            echo json_encode(["success" => 0, "Please fill all required data!"]);
+            echo json_encode(["success" => false, "Please fill all required data!"]);
         }
 
     }
--- controllers/userController.php
+++ controllers/userController.php
@@ -5,10 +5,6 @@ header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// $data_base = new Database();
-
-// $db = $data_base->db();
-
 class userController
 {
 
